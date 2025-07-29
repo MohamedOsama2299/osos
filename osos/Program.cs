1994eb2916953666dd0d8401fd1e7df470647977
mohammedosamahelmy5 <mohammedosamahelmy5@outlook.com>
2025-07-29 17:06:09 +0300

    q1

--- osos/Program.cs
+++ osos/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           int x = 10;
+            int y = 20;
+            Console.WriteLine(x+y);
         }
     }
 }
